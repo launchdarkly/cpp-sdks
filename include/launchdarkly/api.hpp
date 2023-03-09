f8d8ada7471f14ca5c75ec661e5aa5f0d955d603
Casey Waldren <cwaldren@launchdarkly.com>
2023-03-08 17:25:24 -0800

    chore: add initial CMake configuration and basic api.hpp/api.cpp

---
+++ include/launchdarkly/api.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <optional>
+#include <cstdint>
+
+std::optional<std::int32_t> foo();
---
+++ src/api.cpp
@@ -0,0 +1,9 @@
+#include <launchdarkly/api.hpp>
+
+#include <optional>
+#include <cstdint>
+
+std::optional<std::int32_t> foo() {
+  return std::nullopt;
+}
+
