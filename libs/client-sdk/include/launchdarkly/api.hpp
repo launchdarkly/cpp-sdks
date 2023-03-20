e9818bee3ce8ccced9d4f807f931eb7588d2c6a8
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-03-20 15:47:51 -0700

    build: Updates to build on linux.

--- libs/client-sdk/include/launchdarkly/api.hpp
+++ libs/client-sdk/include/launchdarkly/api.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "../../../../../../../../../../Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX13.1.sdk/usr/include/c++/v1/optional"
-#include "../../../../../../../../../../Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX13.1.sdk/usr/include/c++/v1/cstdint"
+#include "optional"
+#include "cstdint"
 
 namespace launchdarkly {
     std::optional<std::int32_t> foo();
