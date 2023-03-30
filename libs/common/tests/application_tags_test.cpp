751876df46cb8126797ae8a4129b4e1bc6ace5f0
Casey Waldren <cwaldren@launchdarkly.com>
2023-03-30 13:35:03 -0700

    Add more tests

--- libs/common/tests/application_tags_test.cpp
+++ libs/common/tests/application_tags_test.cpp
@@ -1,44 +1,99 @@
 
 #include "config/detail/application_tags.hpp"
-
 #include <gtest/gtest.h>
+#include <ostream>
 #include <tuple>
 
-struct TagTest {
+using namespace launchdarkly::config::detail;
+
+struct TagValidity {
     std::string key;
     std::string value;
     bool valid;
 };
 
-class AppTagsFixture : public ::testing::TestWithParam<TagTest> {};
+class TagValidityFixture : public ::testing::TestWithParam<TagValidity> {};
 
 INSTANTIATE_TEST_SUITE_P(
     AppTagsTest,
-    AppTagsFixture,
+    TagValidityFixture,
     testing::Values(
-        TagTest{"", "abc", false},
-        TagTest{" ", "abc", false},
-        TagTest{"/", "abc", false},
-        TagTest{":", "abcs", false},
-        TagTest{"abcABC123.-_", "abc", true},
-        TagTest{"abc", "", false},
-        TagTest{"abc", " ", false},
-        TagTest{"abc", "/", false},
-        TagTest{"abc", ":", false},
-        TagTest{"abc", "abcABC123.-_", true},
-        TagTest{
+        TagValidity{"", "abc", false},
+        TagValidity{" ", "abc", false},
+        TagValidity{"/", "abc", false},
+        TagValidity{":", "abcs", false},
+        TagValidity{"abcABC123.-_", "abc", true},
+        TagValidity{"abc", "", false},
+        TagValidity{"abc", " ", false},
+        TagValidity{"abc", "/", false},
+        TagValidity{"abc", ":", false},
+        TagValidity{"abc", "abcABC123.-_", true},
+        TagValidity{
             "abc",
             "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijkl",
             true,
         },
-        TagTest{"abc",
-                "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghij"
-                "klm",  // > 64 chars
-                false}));
+        TagValidity{
+            "abc",
+            "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghij"
+            "klm",  // > 64 chars
+            false}));
 
-TEST_P(AppTagsFixture, ValidTagValues) {
+TEST_P(TagValidityFixture, ValidTagValues) {
     auto param = GetParam();
     bool valid =
         launchdarkly::config::detail::is_valid_tag(param.key, param.value);
     ASSERT_EQ(valid, param.valid);
+}
+
+struct TagBuild {
+    std::optional<std::string> app_id;
+    std::optional<std::string> app_version;
+    std::optional<std::string> concat;
+    std::string name;
+};
+
+class TagBuildFixture : public ::testing::TestWithParam<TagBuild> {};
+
+auto const kInvalidTag = "!*@!#";
+
+INSTANTIATE_TEST_SUITE_P(
+    AppTagsTest,
+    TagBuildFixture,
+    testing::Values(
+        TagBuild{std::nullopt, std::nullopt, std::nullopt,
+                 "no tags means no output"},
+        TagBuild{"microservice-a", "1.0",
+                 "application-id/microservice-a application-version/1.0",
+                 "multiple tags are ordered correctly"},
+        TagBuild{"microservice-a", std::nullopt,
+                 "application-id/microservice-a",
+                 "single app id tag is output"},
+        TagBuild{std::nullopt, "1.0", "application-version/1.0",
+                 "single app version tag is output"},
+        TagBuild{kInvalidTag, std::nullopt, std::nullopt,
+                 "invalid tag is not output"},
+        TagBuild{kInvalidTag, kInvalidTag, std::nullopt,
+                 "two invalid tags are not output"},
+        TagBuild{"microservice-a", kInvalidTag, "application-id/microservice-a",
+                 "presence of invalid tag does not prevent valid tag from "
+                 "being output"}),
+    [](testing::TestParamInfo<TagBuild> const& info) {
+        std::string name = info.param.name;
+        std::replace_if(
+            name.begin(), name.end(), [](char c) { return c == ' '; }, '_');
+        return "test" + std::to_string(info.index) + "_" + name;
+    });
+
+TEST_P(TagBuildFixture, BuiltTags) {
+    auto p = GetParam();
+
+    ApplicationInfo info;
+    if (p.app_id) {
+        info.app_identifier(*p.app_id);
+    }
+    if (p.app_version) {
+        info.app_version(*p.app_version);
+    }
+    ASSERT_EQ(info.build(), p.concat);
 }
