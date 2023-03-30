861a608e7f4da2c933af1f5828e00cf0a3208dc7
Casey Waldren <cwaldren@launchdarkly.com>
2023-03-30 16:44:06 -0700

    feat: add error.hpp and tl::expected library

--- libs/common/tests/service_endpoints_test.cpp
+++ libs/common/tests/service_endpoints_test.cpp
@@ -2,12 +2,15 @@
 
 #include "config/client.hpp"
 #include "config/server.hpp"
+#include "error.hpp"
 
 class ServiceEndpointTest : public testing::Test {};
 
 using ClientEndpointsBuilder = launchdarkly::client::EndpointsBuilder;
 
 using ServerEndpointsBuilder = launchdarkly::server::EndpointsBuilder;
+
+using launchdarkly::Error;
 
 TEST(ServiceEndpointTest, DefaultClientBuilderURLs) {
     ClientEndpointsBuilder builder;
@@ -28,28 +31,22 @@ TEST(ServiceEndpointTest, DefaultServerBuilderURLs) {
     ASSERT_EQ(eps->events_base_url(), "https://events.launchdarkly.com");
 }
 
-TEST(ServiceEndpointTest, ModifySingleURLCausesError_Polling) {
-    ClientEndpointsBuilder builder;
-    builder.polling_base_url("foo");
-    ASSERT_FALSE(builder.build());
-}
+TEST(ServiceEndpointTest, ModifySingleURLCausesError) {
+    auto result = ClientEndpointsBuilder().polling_base_url("foo").build();
+    ASSERT_FALSE(result);
+    ASSERT_EQ(result.error(), Error::kConfig_Endpoints_AllURLsMustBeSet);
 
-TEST(ServiceEndpointTest, ModifySingleURLCausesError_Streaming) {
-    ClientEndpointsBuilder builder;
-    builder.streaming_base_url("foo");
-    ASSERT_FALSE(builder.build());
-}
+    result = ClientEndpointsBuilder().streaming_base_url("foo").build();
+    ASSERT_FALSE(result);
+    ASSERT_EQ(result.error(), Error::kConfig_Endpoints_AllURLsMustBeSet);
 
-TEST(ServiceEndpointTest, ModifySingleURLCausesError_Events) {
-    ClientEndpointsBuilder builder;
-    builder.events_base_url("foo");
-    ASSERT_FALSE(builder.build());
+    result = ClientEndpointsBuilder().events_base_url("foo").build();
+    ASSERT_FALSE(result);
+    ASSERT_EQ(result.error(), Error::kConfig_Endpoints_AllURLsMustBeSet);
 }
 
 TEST(ServiceEndpointsTest, RelaySetsAllURLS) {
-    ClientEndpointsBuilder builder;
-    builder.relay_proxy("foo");
-    auto eps = builder.build();
+    auto eps = ClientEndpointsBuilder().relay_proxy("foo").build();
     ASSERT_TRUE(eps);
     ASSERT_EQ(eps->streaming_base_url(), "foo");
     ASSERT_EQ(eps->polling_base_url(), "foo");
@@ -58,52 +55,50 @@ TEST(ServiceEndpointsTest, RelaySetsAllURLS) {
 
 TEST(ServiceEndpointsTest, TrimsTrailingSlashes) {
     {
-        ClientEndpointsBuilder builder;
-        builder.relay_proxy("foo/");
-        auto eps = builder.build();
+        auto eps = ClientEndpointsBuilder().relay_proxy("foo/").build();
         ASSERT_TRUE(eps);
         ASSERT_EQ(eps->streaming_base_url(), "foo");
     }
 
     {
-        ClientEndpointsBuilder builder;
-        builder.relay_proxy("foo////////");
-        auto eps = builder.build();
+        auto eps = ClientEndpointsBuilder().relay_proxy("foo////////").build();
         ASSERT_TRUE(eps);
         ASSERT_EQ(eps->streaming_base_url(), "foo");
     }
 
     {
-        ClientEndpointsBuilder builder;
-        builder.relay_proxy("/");
-        auto eps = builder.build();
+        auto eps = ClientEndpointsBuilder().relay_proxy("/").build();
         ASSERT_TRUE(eps);
         ASSERT_EQ(eps->streaming_base_url(), "");
     }
 }
 
 TEST(ServiceEndpointsTest, EmptyURLsAreInvalid) {
-    {
-        ClientEndpointsBuilder builder;
-        builder.relay_proxy("");
-        auto eps = builder.build();
-        ASSERT_FALSE(eps);
-    }
-    {
-        ClientEndpointsBuilder builder;
-        auto eps = builder.streaming_base_url("")
-                       .events_base_url("foo")
-                       .polling_base_url("bar")
-                       .build();
-        ASSERT_FALSE(eps);
-    }
+    auto result = ClientEndpointsBuilder().relay_proxy("").build();
+    ASSERT_FALSE(result);
+    ASSERT_EQ(result.error(), Error::kConfig_Endpoints_EmptyURL);
 
-    {
-        ClientEndpointsBuilder builder;
-        auto eps = builder.streaming_base_url("foo")
-                       .events_base_url("bar")
-                       .polling_base_url("")
-                       .build();
-        ASSERT_FALSE(eps);
-    }
+    result = ClientEndpointsBuilder()
+                 .streaming_base_url("")
+                 .events_base_url("foo")
+                 .polling_base_url("bar")
+                 .build();
+    ASSERT_FALSE(result);
+    ASSERT_EQ(result.error(), Error::kConfig_Endpoints_EmptyURL);
+
+    result = ClientEndpointsBuilder()
+                 .streaming_base_url("foo")
+                 .events_base_url("")
+                 .polling_base_url("bar")
+                 .build();
+    ASSERT_FALSE(result);
+    ASSERT_EQ(result.error(), Error::kConfig_Endpoints_EmptyURL);
+
+    result = ClientEndpointsBuilder()
+                 .streaming_base_url("foo")
+                 .events_base_url("bar")
+                 .polling_base_url("")
+                 .build();
+    ASSERT_FALSE(result);
+    ASSERT_EQ(result.error(), Error::kConfig_Endpoints_EmptyURL);
 }
