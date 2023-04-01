a61e5b4b93e933b66985593441f2761cc0bc8ac3
Casey Waldren <cwaldren@launchdarkly.com>
2023-03-31 17:20:11 -0700

    set port

--- apps/sse-contract-tests/src/main.cpp
+++ apps/sse-contract-tests/src/main.cpp
@@ -19,10 +19,14 @@ int main(int argc, char* argv[]) {
     launchdarkly::Logger logger{
         std::make_unique<ConsoleBackend>("sse-contract-tests")};
 
+    std::string port = "8123";
+    if (argc == 2) {
+        port = argv[1];
+    }
     try {
         net::io_context ioc{1};
 
-        auto s = std::make_shared<server>(ioc, "0.0.0.0", "8111", logger);
+        auto s = std::make_shared<server>(ioc, "0.0.0.0", port, logger);
         s->add_capability("headers");
         s->add_capability("comments");
         s->add_capability("report");
