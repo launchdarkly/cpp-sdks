4602fef15ce335e16956c3626ff4b1ace207515b
Casey Waldren <cwaldren@launchdarkly.com>
2023-05-04 14:21:57 -0700

    point to launchdarkly/foxy repo

--- cmake/foxy.cmake
+++ cmake/foxy.cmake
@@ -3,8 +3,8 @@ cmake_minimum_required(VERSION 3.11)
 include(FetchContent)
 
 FetchContent_Declare(foxy
-        GIT_REPOSITORY https://github.com/cmazakas/foxy.git
-        GIT_TAG 7f4ac0495ad2ed9cd0eca5994743d677ac1d2636
+        GIT_REPOSITORY https://github.com/launchdarkly/foxy.git
+        GIT_TAG cw/add-perpetual-request
         )
 
 set(BUILD_TESTING OFF)
