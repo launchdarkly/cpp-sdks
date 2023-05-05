ad5cda4015b4e77687a8345382530a8a767865b3
Casey Waldren <cwaldren@launchdarkly.com>
2023-05-05 09:43:28 -0700

    fix some CMakeLists.txt CMP0135 errors

--- cmake/expected.cmake
+++ cmake/expected.cmake
@@ -2,6 +2,11 @@ cmake_minimum_required(VERSION 3.11)
 
 include(FetchContent)
 
+if (${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.24")
+    # Affects robustness of timestamp checking on FetchContent dependencies.
+    cmake_policy(SET CMP0135 NEW)
+endif ()
+
 FetchContent_Declare(tl-expected
         GIT_REPOSITORY https://github.com/TartanLlama/expected.git
         GIT_TAG 292eff8bd8ee230a7df1d6a1c00c4ea0eb2f0362
--- cmake/foxy.cmake
+++ cmake/foxy.cmake
@@ -2,6 +2,11 @@ cmake_minimum_required(VERSION 3.11)
 
 include(FetchContent)
 
+if (${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.24")
+    # Affects robustness of timestamp checking on FetchContent dependencies.
+    cmake_policy(SET CMP0135 NEW)
+endif ()
+
 FetchContent_Declare(foxy
         GIT_REPOSITORY https://github.com/launchdarkly/foxy.git
         GIT_TAG 7f4ac0495ad2ed9cd0eca5994743d677ac1d2636
