f1c368b29ad19e66e476b58c6f4a50646e88dd1f
Casey Waldren <cwaldren@launchdarkly.com>
2023-05-05 09:58:30 -0700

    properly disable foxy tests

--- cmake/foxy.cmake
+++ cmake/foxy.cmake
@@ -7,10 +7,14 @@ if (${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.24")
     cmake_policy(SET CMP0135 NEW)
 endif ()
 
+
 FetchContent_Declare(foxy
         GIT_REPOSITORY https://github.com/launchdarkly/foxy.git
         GIT_TAG 7f4ac0495ad2ed9cd0eca5994743d677ac1d2636
         )
 
-set(BUILD_TESTING OFF)
+# Disable testing for foxy; can be enabled if we bring in Catch2 dependency.
+get_property(ORIGINAL_BUILD_TESTING GLOBAL PROPERTY BUILD_TESTING)
+set(BUILD_TESTING OFF CACHE BOOL "Disable building tests for foxy" FORCE)
 FetchContent_MakeAvailable(foxy)
+set_property(GLOBAL PROPERTY BUILD_TESTING "${ORIGINAL_BUILD_TESTING}")
