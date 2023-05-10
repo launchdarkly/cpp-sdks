72b399423ec6b6455a3b443e7ebab9851d3e7fc1
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-05-10 13:48:08 -0700

    Reset release. Reduce build.

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -15,12 +15,12 @@ cmake --install .
 cd ..
 
 # Build a dynamic release.
-mkdir -p build-dynamic && cd build-dynamic
-mkdir -p release
-cmake -G Ninja -D CMAKE_BUILD_TYPE=Release -D BUILD_TESTING=OFF -D BUILD_SHARED_LIBS=ON -D CMAKE_INSTALL_PREFIX=./release ..
-
-cmake --build . --target "$1"
-cmake --install .
-cd ..
+#mkdir -p build-dynamic && cd build-dynamic
+#mkdir -p release
+#cmake -G Ninja -D CMAKE_BUILD_TYPE=Release -D BUILD_TESTING=OFF -D BUILD_SHARED_LIBS=ON -D CMAKE_INSTALL_PREFIX=./release ..
+#
+#cmake --build . --target "$1"
+#cmake --install .
+#cd ..
 
 
