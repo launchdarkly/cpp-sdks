4320ca95a408e6101e5a9af08b2156641d81ecee
Casey Waldren <cwaldren@launchdarkly.com>
2023-05-23 12:03:23 -0700

    chore: update hello apps to standard format

--- apps/hello-c/main.c
+++ apps/hello-c/main.c
@@ -3,76 +3,64 @@
 #include <launchdarkly/bindings/c/config/builder.h>
 #include <launchdarkly/bindings/c/context_builder.h>
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// Set MOBILE_KEY to your LaunchDarkly mobile key.
+#define MOBILE_KEY ""
+
+// Set FEATURE_FLAG_KEY to the feature flag key you want to evaluate.
+#define FEATURE_FLAG_KEY "my-boolean-flag"
 
 int main() {
-    char const* key = getenv("STG_SDK_KEY");
-    if (!key) {
-        printf("Set environment variable STG_SDK_KEY to the sdk key\n");
+    if (!strlen(MOBILE_KEY)) {
+        printf(
+            "*** Please edit main.c to set MOBILE_KEY to your LaunchDarkly "
+            "mobile key first\n\n");
         return 1;
     }
 
-    LDClientConfigBuilder builder = LDClientConfigBuilder_New(key);
-
-    LDClientConfigBuilder_ServiceEndpoints_PollingBaseURL(
-        builder, "http://sdk.launchdarkly.com");
-    LDClientConfigBuilder_ServiceEndpoints_StreamingBaseURL(
-        builder, "https://stream.launchdarkly.com");
-    LDClientConfigBuilder_ServiceEndpoints_EventsBaseURL(
-        builder, "https://events.launchdarkly.com");
-
-    LDClientConfigBuilder_Events_FlushIntervalMs(builder, 5000);
-
-    LDClientConfigBuilder_DataSource_UseReport(builder, true);
-    LDClientConfigBuilder_DataSource_WithReasons(builder, true);
-
-    LDDataSourcePollBuilder poll_builder = LDDataSourcePollBuilder_New();
-    LDDataSourcePollBuilder_IntervalS(poll_builder, 30);
-    LDClientConfigBuilder_DataSource_MethodPoll(builder, poll_builder);
+    LDClientConfigBuilder config_builder =
+        LDClientConfigBuilder_New(MOBILE_KEY);
 
     LDClientConfig config;
-    LDStatus status = LDClientConfigBuilder_Build(builder, &config);
-
-    if (!LDStatus_Ok(status)) {
-        printf("%s\n", LDStatus_Error(status));
+    LDStatus config_status =
+        LDClientConfigBuilder_Build(config_builder, &config);
+    if (!LDStatus_Ok(config_status)) {
+        printf("error: config is invalid: %s", LDStatus_Error(config_status));
         return 1;
     }
 
     LDContextBuilder context_builder = LDContextBuilder_New();
-    LDContextBuilder_AddKind(context_builder, "user", "ryan");
-
+    LDContextBuilder_AddKind(context_builder, "user", "example-user-key");
+    LDContextBuilder_Attributes_SetName(context_builder, "user", "Sandy");
     LDContext context = LDContextBuilder_Build(context_builder);
 
     LDClientSDK client = LDClientSDK_New(config, context);
-    //
-    //    std::cout << "Initial Status: " << client.DataSourceStatus().Status()
-    //              << std::endl;
-    //
-    //    client.DataSourceStatus().OnDataSourceStatusChange([](auto status) {
-    //        std::cout << "Got status: " << status << std::endl;
-    //    });
-    //
-    //    client.FlagNotifier().OnFlagChange("my-boolean-flag", [](auto event) {
-    //        std::cout << "Got flag change: " << *event << std::endl;
-    //    });
-    //
-    //    client.WaitForReadySync(std::chrono::seconds(30));
 
-    LDEvalDetail detail;
-    if (LDClientSDK_BoolVariationDetail(client, "my-boolean-flag", false,
-                                        &detail)) {
-        printf("Value was: true\n");
+    if (LDClientIsInitialized(client)) {
+        printf("*** SDK successfully initialized!\n\n");
     } else {
-        printf("Value was: false\n");
+        printf("*** SDK failed to initialize\n\n");
+        return 1;
     }
 
-    LDEvalReason reason;
-    if (LDEvalDetail_Reason(detail, &reason)) {
-        printf("Reason was: %d\n", LDEvalReason_Kind(reason));
-    }
+    bool flag_value =
+        LDClientSDK_BoolVariation(client, FEATURE_FLAG_KEY, false);
 
-    // Sit around.
-    printf("Press enter to exit\n\n");
-    getchar();
+    printf("*** Feature flag '%s' is %s for this user\n\n", FEATURE_FLAG_KEY,
+           flag_value ? "true" : "false");
+
+    // Here we ensure that the SDK shuts down cleanly and has a chance to
+    // deliver analytics events to LaunchDarkly before the program exits. If
+    // analytics events are not delivered, the user properties and flag usage
+    // statistics will not appear on your dashboard. In a normal long-running
+    // application, the SDK would continue running and events would be delivered
+    // automatically in the background.
+
+    LDClientSDK_Free(client);
+
+    return 0;
 }
--- apps/hello-cpp/main.cpp
+++ apps/hello-cpp/main.cpp
@@ -1,132 +1,45 @@
 #include <launchdarkly/client_side/client.hpp>
-
-#include <boost/asio/io_context.hpp>
-
 #include <launchdarkly/context_builder.hpp>
 
-#include <filesystem>
-#include <fstream>
 #include <iostream>
-#include <sstream>
 
-namespace net = boost::asio;  // from <boost/asio.hpp>
+// Set MOBILE_KEY to your LaunchDarkly mobile key.
+#define MOBILE_KEY ""
 
-using launchdarkly::ContextBuilder;
-using launchdarkly::LogLevel;
-using launchdarkly::client_side::Client;
-using launchdarkly::client_side::ConfigBuilder;
-using launchdarkly::client_side::DataSourceBuilder;
-using launchdarkly::client_side::PersistenceBuilder;
-using launchdarkly::config::shared::builders::LoggingBuilder;
+// Set FEATURE_FLAG_KEY to the feature flag key you want to evaluate.
+#define FEATURE_FLAG_KEY "my-boolean-flag"
 
-class FilePersistence : public IPersistence {
-   public:
-    FilePersistence(std::string directory) : directory_(std::move(directory)) {
-        std::filesystem::create_directories(directory_);
-    }
-    void Set(std::string storage_namespace,
-             std::string key,
-             std::string data) noexcept override {
-        try {
-            std::ofstream file;
-            file.open(MakePath(storage_namespace, key));
-            file << data;
-            file.close();
-        } catch (...) {
-            std::cout << "Problem writing" << std::endl;
-        }
-    }
-
-    void Remove(std::string storage_namespace,
-                std::string key) noexcept override {
-        std::filesystem::remove(MakePath(storage_namespace, key));
-    }
-
-    std::optional<std::string> Read(std::string storage_namespace,
-                                    std::string key) noexcept override {
-        auto path = MakePath(storage_namespace, key);
-
-        try {
-            if (std::filesystem::exists(path)) {
-                std::ifstream file(path);
-                std::stringstream buffer;
-                buffer << file.rdbuf();
-                return buffer.str();
-            }
-        } catch (...) {
-            std::cout << "Problem reading" << std::endl;
-        }
-        return std::nullopt;
-    }
-
-   private:
-    std::string MakePath(std::string storage_namespace, std::string key) {
-        return directory_ + "/" + storage_namespace + "_" + key;
-    }
-    std::string directory_;
-};
-
+using namespace launchdarkly;
 int main() {
-    net::io_context ioc;
-
-    char const* key = std::getenv("STG_SDK_KEY");
-    if (!key) {
-        std::cout << "Set environment variable STG_SDK_KEY to the sdk key"
-                  << std::endl;
+    if (!strlen(MOBILE_KEY)) {
+        printf(
+            "*** Please edit main.c to set MOBILE_KEY to your LaunchDarkly "
+            "mobile key first\n\n");
         return 1;
     }
 
-    auto config_builder = ConfigBuilder(key);
-
-    config_builder.ServiceEndpoints()
-        .PollingBaseUrl("http://sdk.launchdarkly.com")
-        .StreamingBaseUrl("https://stream.launchdarkly.com")
-        .EventsBaseUrl("https://events.launchdarkly.com");
-    config_builder.DataSource()
-        .Method(
-            DataSourceBuilder::Polling().PollInterval(std::chrono::seconds{30}))
-        .WithReasons(true)
-        .UseReport(true);
-    config_builder.Logging().Logging(
-        LoggingBuilder::BasicLogging().Level(LogLevel::kDebug));
-    config_builder.Events().FlushInterval(std::chrono::seconds(5));
-    config_builder.Persistence().Custom(
-        std::make_shared<FilePersistence>("ld_persist"));
-
-    auto config = config_builder.Build();
+    auto config = client_side::ConfigBuilder(MOBILE_KEY).Build();
     if (!config) {
-        std::cout << config.error();
+        std::cout << "error: config is invalid: " << config.error() << '\n';
         return 1;
     }
 
-    Client client(std::move(*config),
-                  ContextBuilder().kind("user", "ryan").build());
+    auto context =
+        ContextBuilder().kind("user", "example-user-key").name("Sandy").build();
 
-    auto before_init = client.BoolVariationDetail("my-boolean-flag", false);
-    // This should be the cached version from our persistence, if the
-    // persistence is populated.
-    std::cout << "Before Init Complete: " << *before_init << std::endl;
+    auto client = client_side::Client(std::move(*config), std::move(context));
 
-    std::cout << "Initial Status: " << client.DataSourceStatus().Status()
-              << std::endl;
-
-    client.DataSourceStatus().OnDataSourceStatusChange([](auto status) {
-        std::cout << "Got status: " << status << std::endl;
-    });
-
-    client.FlagNotifier().OnFlagChange("my-boolean-flag", [](auto event) {
-        std::cout << "Got flag change: " << *event << std::endl;
-    });
-
-    client.WaitForReadySync(std::chrono::seconds(30));
-
-    auto value = client.BoolVariationDetail("my-boolean-flag", false);
-    std::cout << "Value was: " << *value << std::endl;
-    if (auto reason = value.Reason()) {
-        std::cout << "Reason was: " << *reason << std::endl;
+    if (client.Initialized()) {
+        std::cout << "*** SDK successfully initialized!\n\n";
+    } else {
+        std::cout << "*** SDK failed to initialize\n\n";
+        return 1;
     }
 
-    // Sit around.
-    std::cout << "Press enter to exit" << std::endl << std::endl;
-    std::cin.get();
+    bool flag_value = client.BoolVariation(FEATURE_FLAG_KEY, false);
+
+    std::cout << "*** Feature flag '" << FEATURE_FLAG_KEY << "' is "
+              << (flag_value ? "true" : "false") << " for this user\n\n";
+
+    return 0;
 }
