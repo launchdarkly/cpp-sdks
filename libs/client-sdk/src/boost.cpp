4cea9fc4b9e711a54c5a47db5ed893a9a7e15b5d
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-05-22 17:21:53 -0700

    cleanup

--- libs/client-sdk/src/boost.cpp
+++ libs/client-sdk/src/boost.cpp
@@ -1,2 +1,5 @@
+// This file is used to include boost url/json when building a shared library on linux/mac.
+// Windows links static libs in this case and does not include these src files, as there
+// are issues compiling the value.ipp file from JSON with MSVC.
 #include <boost/url/src.hpp>
 #include <boost/json/src.hpp>
