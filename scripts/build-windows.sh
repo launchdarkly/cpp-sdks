1604b01b9b47aafdcb944c93e515fdaca119f445
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-05-24 14:50:13 -0700

    chore: Include debug artifacts in windows install.

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -13,11 +13,18 @@ mkdir -p release
 cmake -G Ninja -D CMAKE_BUILD_TYPE=Debug -D BUILD_TESTING=OFF -D CMAKE_INSTALL_PREFIX=./release ..
 
 cmake --build . --target "$1"
+
+cmake --build . --target "$1"
+cmake --install .
+
 cd ..
 
 # Build a dynamic debug release.
 mkdir -p build-dynamic-debug && cd build-dynamic-debug
 mkdir -p release
 cmake -G Ninja -D CMAKE_BUILD_TYPE=Debug -D BUILD_TESTING=OFF -D BUILD_SHARED_LIBS=ON -D CMAKE_INSTALL_PREFIX=./release ..
+
+cmake --build . --target "$1"
+cmake --install .
 
 cmake --build . --target "$1"
