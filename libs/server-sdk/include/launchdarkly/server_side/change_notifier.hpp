54499d667ccc26f5045df0648bda9827e525c64c
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-07-07 08:40:28 -0700

    Start adding change notifier.

---
+++ libs/server-sdk/include/launchdarkly/server_side/change_notifier.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <launchdarkly/connection.hpp>
+
+#include <functional>
+#include <memory>
+#include <set>
+#include <string>
+
+namespace launchdarkly::server_side {
+
+/**
+ * Interface to allow listening for flag changes. Notification events should
+ * be distributed after the store has been updated.
+ */
+class IChangeNotifier {
+   public:
+    using ChangeSet = std::set<std::string>;
+    using ChangeHandler = std::function<void(std::shared_ptr<ChangeSet>)>;
+
+    /**
+     * Listen for changes to flag configuration. The change handler will be called
+     * with a set of affected flag keys. Changes include flags whose dependencies
+     * (either other flags, or segments) changed.
+     *
+     * @param signal The handler for the changes.
+     * @return A connection which can be used to stop listening.
+     */
+    virtual std::unique_ptr<IConnection> OnFlagChange(ChangeHandler handler) = 0;
+
+    virtual ~IChangeNotifier() = default;
+    IChangeNotifier(IChangeNotifier const& item) = delete;
+    IChangeNotifier(IChangeNotifier&& item) = delete;
+    IChangeNotifier& operator=(IChangeNotifier const&) = delete;
+    IChangeNotifier& operator=(IChangeNotifier&&) = delete;
+
+   protected:
+    IChangeNotifier() = default;
+};
+
+}  // namespace launchdarkly::server_side
--- libs/server-sdk/src/data_store/data_store_updater.hpp
+++ libs/server-sdk/src/data_store/data_store_updater.hpp
@@ -1,7 +1,16 @@
 #pragma once
 
-namespace launchdarkly::server_side::data_store {
-class DataStoreUpdater {
+#include "../data_source/data_source_update_sink.hpp"
 
+#include <launchdarkly/server_side/change_notifier.hpp>
+
+namespace launchdarkly::server_side::data_store {
+class DataStoreUpdater
+    : public launchdarkly::server_side::data_source::IDataSourceUpdateSink,
+      public launchdarkly::server_side::IChangeNotifier {
+   public:
+    void Init(launchdarkly::data_model::SDKDataSet dataSet) override;
+    void Upsert(std::string key, FlagDescriptor flag) override;
+    void Upsert(std::string key, SegmentDescriptor segment) override;
 };
 }  // namespace launchdarkly::server_side::data_store
