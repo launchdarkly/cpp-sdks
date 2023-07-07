b53b004e3347248aa6f078710064942154e321ab
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-07-07 09:54:51 -0700

    Basic memory store functionaliy.

--- libs/server-sdk/src/data_store/memory_store.cpp
+++ libs/server-sdk/src/data_store/memory_store.cpp
@@ -6,31 +6,66 @@ namespace launchdarkly::server_side::data_store {
 
 std::shared_ptr<IDataStore::FlagDescriptor> MemoryStore::GetFlag(
     std::string key) const {
+    std::lock_guard lock{data_mutex_};
     return std::shared_ptr<FlagDescriptor>();
 }
 
 std::shared_ptr<IDataStore::SegmentDescriptor> MemoryStore::GetSegment(
     std::string key) const {
+    std::lock_guard lock{data_mutex_};
     return std::shared_ptr<SegmentDescriptor>();
 }
 
 std::unordered_map<std::string, std::shared_ptr<IDataStore::FlagDescriptor>>
 MemoryStore::AllFlags() const {
-    return std::unordered_map<std::string, std::shared_ptr<FlagDescriptor>>();
+    std::lock_guard lock{data_mutex_};
+    return {flags_};
 }
 
 std::unordered_map<std::string, std::shared_ptr<IDataStore::SegmentDescriptor>>
 MemoryStore::AllSegments() const {
-    return std::unordered_map<std::string,
-                              std::shared_ptr<IDataStore::SegmentDescriptor>>();
+    std::lock_guard lock{data_mutex_};
+    return {segments_};
 }
 
 bool MemoryStore::Initialized() const {
-    return false;
+    std::lock_guard lock{data_mutex_};
+    return initialized_;
 }
 
 std::string const& MemoryStore::Description() const {
     return description_;
+}
+
+void MemoryStore::Init(launchdarkly::data_model::SDKDataSet dataSet) {
+    std::lock_guard lock{data_mutex_};
+    initialized_ = true;
+    flags_.clear();
+    segments_.clear();
+    for (auto flag : dataSet.flags) {
+        flags_.emplace(flag.first, std::make_shared<IDataStore::FlagDescriptor>(
+                                       std::move(flag.second)));
+    }
+    for (auto segment : dataSet.segments) {
+        segments_.emplace(segment.first,
+                          std::make_shared<IDataStore::SegmentDescriptor>(
+                              std::move(segment.second)));
+    }
+}
+
+void MemoryStore::Upsert(
+    std::string key,
+    data_source::IDataSourceUpdateSink::FlagDescriptor flag) {
+    std::lock_guard lock{data_mutex_};
+    flags_[key] = std::make_shared<IDataStore::FlagDescriptor>(std::move(flag));
+}
+
+void MemoryStore::Upsert(
+    std::string key,
+    data_source::IDataSourceUpdateSink::SegmentDescriptor segment) {
+    std::lock_guard lock{data_mutex_};
+    segments_[key] =
+        std::make_shared<IDataStore::SegmentDescriptor>(std::move(segment));
 }
 
 }  // namespace launchdarkly::server_side::data_store
--- libs/server-sdk/src/data_store/memory_store.hpp
+++ libs/server-sdk/src/data_store/memory_store.hpp
@@ -1,27 +1,43 @@
 #pragma once
 
+#include "../data_source/data_source_update_sink.hpp"
 #include "data_store.hpp"
 
 #include <memory>
+#include <mutex>
 #include <string>
+#include <unordered_map>
 
 namespace launchdarkly::server_side::data_store {
 
-class MemoryStore : public IDataStore {
+class MemoryStore : public IDataStore,
+                    public data_source::IDataSourceUpdateSink {
    public:
     std::shared_ptr<FlagDescriptor> GetFlag(std::string key) const override;
     std::shared_ptr<SegmentDescriptor> GetSegment(
         std::string key) const override;
+
     std::unordered_map<std::string, std::shared_ptr<FlagDescriptor>> AllFlags()
         const override;
     std::unordered_map<std::string, std::shared_ptr<SegmentDescriptor>>
     AllSegments() const override;
+
     bool Initialized() const override;
     std::string const& Description() const override;
+
+    void Init(launchdarkly::data_model::SDKDataSet dataSet) override;
+    void Upsert(std::string key, FlagDescriptor flag) override;
+    void Upsert(std::string key, SegmentDescriptor segment) override;
+
     ~MemoryStore() override = default;
 
    private:
     static inline std::string description_ = "Memory";
+    std::unordered_map<std::string, std::shared_ptr<FlagDescriptor>> flags_;
+    std::unordered_map<std::string, std::shared_ptr<SegmentDescriptor>>
+        segments_;
+    bool initialized_ = false;
+    mutable std::mutex data_mutex_;
 };
 
 }  // namespace launchdarkly::server_side::data_store
