276e2226991bd4ad6792479adefc277afb538052
Casey Waldren <cwaldren@launchdarkly.com>
2023-07-12 11:02:09 -0700

    feat: add sha1 hashing  and base16 encoding routines (#170)
    Adds a new `sha1` hash function, implemented similar to the existing
    `sha256`.
    
    Moved the hex encoding routine from sha256's tests into a
    dedicated header so it can be reused in sha1's tests or elsewhere.

---
+++ libs/internal/include/launchdarkly/encoding/base_16.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iomanip>
+#include <string>
+
+namespace launchdarkly::encoding {
+
+template <unsigned long N>
+std::string Base16Encode(std::array<unsigned char, N> arr) {
+    std::stringstream output_stream;
+    output_stream << std::hex << std::noshowbase;
+    for (unsigned char byte : arr) {
+        output_stream << std::setw(2) << std::setfill('0')
+                      << static_cast<int>(byte);
+    }
+    return output_stream.str();
+}
+
+}  // namespace launchdarkly::encoding
