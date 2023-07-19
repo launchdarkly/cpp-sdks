1fee5a48d27804931def832cacded020348e51f1
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-07-19 13:55:31 -0700

    fix: Build and test fixes.

--- libs/server-sdk/src/evaluation/evaluation_error.hpp
+++ libs/server-sdk/src/evaluation/evaluation_error.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <optional>
 #include <ostream>
 
 namespace launchdarkly::server_side::evaluation {
--- libs/server-sdk/tests/evaluation_stack_test.cpp
+++ libs/server-sdk/tests/evaluation_stack_test.cpp
@@ -4,50 +4,55 @@
 
 using namespace launchdarkly::server_side::evaluation::detail;
 
+struct TestString {
+    static const inline std::string foo = "foo";
+    static const inline std::string bar = "bar";
+};
+
 TEST(EvalStackTests, SegmentIsNoticed) {
     EvaluationStack stack;
-    auto g1 = stack.NoticeSegment("foo");
+    auto g1 = stack.NoticeSegment(TestString::foo);
     ASSERT_TRUE(g1);
-    ASSERT_FALSE(stack.NoticeSegment("foo"));
+    ASSERT_FALSE(stack.NoticeSegment(TestString::foo));
 }
 
 TEST(EvalStackTests, PrereqIsNoticed) {
     EvaluationStack stack;
-    auto g1 = stack.NoticePrerequisite("foo");
+    auto g1 = stack.NoticePrerequisite(TestString::foo);
     ASSERT_TRUE(g1);
-    ASSERT_FALSE(stack.NoticePrerequisite("foo"));
+    ASSERT_FALSE(stack.NoticePrerequisite(TestString::foo));
 }
 
 TEST(EvalStackTests, NestedScopes) {
     EvaluationStack stack;
     {
-        auto g1 = stack.NoticeSegment("foo");
+        auto g1 = stack.NoticeSegment(TestString::foo);
         ASSERT_TRUE(g1);
-        ASSERT_FALSE(stack.NoticeSegment("foo"));
+        ASSERT_FALSE(stack.NoticeSegment(TestString::foo));
         {
-            auto g2 = stack.NoticeSegment("bar");
+            auto g2 = stack.NoticeSegment(TestString::bar);
             ASSERT_TRUE(g2);
-            ASSERT_FALSE(stack.NoticeSegment("bar"));
-            ASSERT_FALSE(stack.NoticeSegment("foo"));
+            ASSERT_FALSE(stack.NoticeSegment(TestString::bar));
+            ASSERT_FALSE(stack.NoticeSegment(TestString::foo));
         }
-        ASSERT_TRUE(stack.NoticeSegment("bar"));
+        ASSERT_TRUE(stack.NoticeSegment(TestString::bar));
     }
-    ASSERT_TRUE(stack.NoticeSegment("foo"));
-    ASSERT_TRUE(stack.NoticeSegment("bar"));
+    ASSERT_TRUE(stack.NoticeSegment(TestString::foo));
+    ASSERT_TRUE(stack.NoticeSegment(TestString::bar));
 }
 
 TEST(EvalStackTests, SegmentAndPrereqHaveSeparateCaches) {
     EvaluationStack stack;
-    auto g1 = stack.NoticeSegment("foo");
+    auto g1 = stack.NoticeSegment(TestString::foo);
     ASSERT_TRUE(g1);
-    auto g2 = stack.NoticePrerequisite("foo");
+    auto g2 = stack.NoticePrerequisite(TestString::foo);
     ASSERT_TRUE(g2);
 }
 
 TEST(EvalStackTests, ImmediateDestructionOfGuard) {
     EvaluationStack stack;
 
-    ASSERT_TRUE(stack.NoticeSegment("foo"));
-    ASSERT_TRUE(stack.NoticeSegment("foo"));
-    ASSERT_TRUE(stack.NoticeSegment("foo"));
+    ASSERT_TRUE(stack.NoticeSegment(TestString::foo));
+    ASSERT_TRUE(stack.NoticeSegment(TestString::foo));
+    ASSERT_TRUE(stack.NoticeSegment(TestString::foo));
 }
