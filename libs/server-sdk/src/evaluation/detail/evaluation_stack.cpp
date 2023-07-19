0ead7055a6f1ae81f921b608ba2d48002448a7ef
Casey Waldren <cwaldren@launchdarkly.com>
2023-07-19 14:29:50 -0700

    fix: EvaluationStack should take ownership of key argument (#190)
    Current API of `EvaluationStack` is very easy to accidentally misuse
    because it takes a reference to its key argument. If the arg is
    destroyed, then we'll have a use-after-free.

--- libs/server-sdk/src/evaluation/detail/evaluation_stack.cpp
+++ libs/server-sdk/src/evaluation/detail/evaluation_stack.cpp
@@ -2,8 +2,8 @@
 
 namespace launchdarkly::server_side::evaluation::detail {
 
-Guard::Guard(std::unordered_set<std::string>& set, std::string const& key)
-    : set_(set), key_(key) {
+Guard::Guard(std::unordered_set<std::string>& set, std::string key)
+    : set_(set), key_(std::move(key)) {
     set_.insert(key_);
 }
 
@@ -12,19 +12,19 @@ Guard::~Guard() {
 }
 
 std::optional<Guard> EvaluationStack::NoticePrerequisite(
-    std::string const& prerequisite_key) {
+    std::string prerequisite_key) {
     if (prerequisites_seen_.count(prerequisite_key) != 0) {
         return std::nullopt;
     }
-    return std::make_optional<Guard>(prerequisites_seen_, prerequisite_key);
+    return std::make_optional<Guard>(prerequisites_seen_,
+                                     std::move(prerequisite_key));
 }
 
-std::optional<Guard> EvaluationStack::NoticeSegment(
-    std::string const& segment_key) {
+std::optional<Guard> EvaluationStack::NoticeSegment(std::string segment_key) {
     if (segments_seen_.count(segment_key) != 0) {
         return std::nullopt;
     }
-    return std::make_optional<Guard>(segments_seen_, segment_key);
+    return std::make_optional<Guard>(segments_seen_, std::move(segment_key));
 }
 
 }  // namespace launchdarkly::server_side::evaluation::detail
