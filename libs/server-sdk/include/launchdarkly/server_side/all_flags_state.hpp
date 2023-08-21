796ff708a35af868335ee54091569759f917799c
Casey Waldren <cwaldren@launchdarkly.com>
2023-08-21 13:57:15 -0700

    doc comments on AllFlagsState

--- libs/server-sdk/include/launchdarkly/server_side/all_flags_state.hpp
+++ libs/server-sdk/include/launchdarkly/server_side/all_flags_state.hpp
@@ -9,8 +9,19 @@
 
 namespace launchdarkly::server_side {
 
+/**
+ * AllFlagsState is a snapshot of the state of multiple feature flags with
+ * regard to a specific evaluation context.
+ *
+ * Serializing this object to JSON using boost::json::value_from will produce
+ * the appropriate data structure for bootstrapping the LaunchDarkly JavaScript
+ * client.
+ */
 class AllFlagsState {
    public:
+    /**
+     * Metadata contains information pertaining to a single feature flag.
+     */
     class Metadata {
        public:
         Metadata(std::uint64_t version,
@@ -20,13 +31,52 @@ class AllFlagsState {
                  bool track_reason,
                  std::optional<std::uint64_t> debug_events_until_date);
 
+        /**
+         * @return The flag's version number when it was evaluated.
+         */
         [[nodiscard]] std::uint64_t Version() const;
+
+        /**
+         * @return The variation index that was selected for the specified
+         * evaluation context.
+         */
         [[nodiscard]] std::optional<std::int64_t> Variation() const;
+
+        /**
+         * @return The reason that the flag evaluation produced the specified
+         * variation.
+         */
         [[nodiscard]] std::optional<EvaluationReason> const& Reason() const;
+
+        /**
+         * @return True if a full feature event must be sent when evaluating
+         * this flag. This will be true if tracking was explicitly enabled for
+         * this flag for data export, or if the evaluation involved an
+         * experiment, or both.
+         */
         [[nodiscard]] bool TrackEvents() const;
+
+        /**
+         * @return True if the evaluation reason should always be included in
+         * any full feature event created for this flag, regardless of whether a
+         * VariationDetail method was called. This will be true if the
+         * evaluation involved an experiment.
+         */
         [[nodiscard]] bool TrackReason() const;
+
+        /**
+         * @return The date on which debug mode expires for this flag, if
+         * enabled.
+         */
         [[nodiscard]] std::optional<std::uint64_t> const& DebugEventsUntilDate()
             const;
+
+        /**
+         *
+         * @return True if the options passed to AllFlagsState, combined with
+         * the obtained flag state, indicate that some metadata can be left out
+         * of the JSON serialization.
+         */
         [[nodiscard]] bool OmitDetails() const;
 
         friend class AllFlagsStateBuilder;
@@ -41,10 +91,22 @@ class AllFlagsState {
         bool omit_details_;
     };
 
+    /**
+     * @return True if the call to AllFlagsState succeeded. False if there was
+     * an error, such as the data store being unavailable. When false, the other
+     * accessors will return empty maps.
+     */
     [[nodiscard]] bool Valid() const;
 
+    /**
+     * @return A map of metadata for each flag.
+     */
     [[nodiscard]] std::unordered_map<std::string, Metadata> const& FlagsState()
         const;
+
+    /**
+     * @return A map of evaluation results for each flag.
+     */
     [[nodiscard]] std::unordered_map<std::string, Value> const& Evaluations()
         const;
 
