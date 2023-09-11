dafb787237d71e6c597a2daa7cb0d0b588d1c999
Casey Waldren <cwaldren@launchdarkly.com>
2023-09-11 13:56:02 -0700

    disable tl::expected tests in CI

--- cmake/expected.cmake
+++ cmake/expected.cmake
@@ -7,11 +7,10 @@ if (${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.24")
     cmake_policy(SET CMP0135 NEW)
 endif ()
 
+set(EXPECTED_BUILD_TESTS OFF)
 FetchContent_Declare(tl-expected
         GIT_REPOSITORY https://github.com/TartanLlama/expected.git
         GIT_TAG 292eff8bd8ee230a7df1d6a1c00c4ea0eb2f0362
 )
 
 FetchContent_MakeAvailable(tl-expected)
-
-set_property(TARGET expected PROPERTY COMPILE_WARNINGS_AS_ERROR FALSE)
