7a01f12bd2175fe6d411ec4b8dd56422c8e480d0
Casey Waldren <cwaldren@launchdarkly.com>
2023-10-10 15:33:22 -0700

    add launchdarkly::unreachable in json_value.cpp tag invoke

--- libs/internal/src/serialization/json_value.cpp
+++ libs/internal/src/serialization/json_value.cpp
@@ -1,3 +1,4 @@
+#include <launchdarkly/detail/unreachable.hpp>
 #include <launchdarkly/serialization/json_value.hpp>
 #include <launchdarkly/serialization/value_mapping.hpp>
 
@@ -48,7 +49,7 @@ Value tag_invoke(boost::json::value_to_tag<launchdarkly::Value> const& unused,
     }
     // The above switch is exhaustive, so this can only happen if a new
     // type is added to boost::json::value.
-    assert(!"All types need to be handled.");
+    launchdarkly::detail::unreachable();
 }
 
 void tag_invoke(boost::json::value_from_tag const&,
