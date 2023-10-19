8dd473f825d4d05f1bc4f94621f7e4a4fefab929
Casey Waldren <cwaldren@launchdarkly.com>
2023-10-19 08:59:29 -0700

    fix: LD_BUILD_SHARED_LIBS build flag usage (#260)
    In my previous PR cleaning up cmake variables, during development I
    renamed the `BUILD_SHARED_LIBS` flag to `BUILD_STATIC_LIBS`, but then
    reconsidered and stuck with the original naming.
    
    Unfortunately I didn't un-rename some usages of it - so we ended up
    having some instances of `LD_BUILD_STATIC_LIBS` in our `CMakeLists.txt`.
    
    This removes usage of `LD_BUILD_STATIC_LIBS`.

--- cmake/certify.cmake
+++ cmake/certify.cmake
@@ -10,16 +10,14 @@ endif ()
 FetchContent_Declare(boost_certify
         GIT_REPOSITORY https://github.com/djarek/certify.git
         GIT_TAG 97f5eebfd99a5d6e99d07e4820240994e4e59787
-        )
+)
 
 set(BUILD_TESTING OFF)
 
 FetchContent_GetProperties(boost_certify)
-if(NOT boost_certify_POPULATED)
+if (NOT boost_certify_POPULATED)
     FetchContent_Populate(boost_certify)
     add_subdirectory(${boost_certify_SOURCE_DIR} ${boost_certify_BINARY_DIR} EXCLUDE_FROM_ALL)
-endif()
+endif ()
 
 set(BUILD_TESTING "${ORIGINAL_BUILD_TESTING}")
-
-set(BUILD_SHARED_LIBS "${ORIGINAL_BUILD_SHARED_LIBS}")
