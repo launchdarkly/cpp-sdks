17fe520147af4ee19a53b187712c61cbe75ce1d2
Casey Waldren <cwaldren@launchdarkly.com>
2023-11-17 14:56:23 -0800

    remove incomplete work

--- libs/server-sdk/src/data_components/serialization_adapters/json_pull_source.cpp
+++ libs/server-sdk/src/data_components/serialization_adapters/json_pull_source.cpp
@@ -26,26 +26,12 @@ JsonSource::GetSegment(std::string const& key) const {
 data_interfaces::IPullSource::AllResult<data_model::FlagDescriptor>
 JsonSource::AllFlags() const {
     // TODO: deserialize then return
-    data_interfaces::ISerializedDataPullSource::AllResult result =
-        source_.All(flag_kind_);
-    if (!result) {
-        return tl::make_unexpected(result.error().message);
-    }
 
-    AllResult<data_model::FlagDescriptor> flags;
-    for (auto [key, val] : *result) {
-        auto deserialized = Deserialize<data_model::Flag>(std::move(val));
-
-    }
-    return flags;
 }
 
 data_interfaces::IPullSource::AllResult<data_model::SegmentDescriptor>
 JsonSource::AllSegments() const {
     // TODO: deserialize then return
-
-    data_interfaces::ISerializedDataPullSource::AllResult result =
-        source_.All(segment_kind_);
 }
 
 std::string const& JsonSource::Identity() const {
