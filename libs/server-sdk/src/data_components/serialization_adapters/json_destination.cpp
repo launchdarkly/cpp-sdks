c3fb9bebc8306f70dff868f6fa54655f6c8e4673
Casey Waldren <cwaldren@launchdarkly.com>
2023-11-22 12:35:08 -0800

    implementing JSON destination

--- libs/server-sdk/src/data_components/serialization_adapters/json_destination.cpp
+++ libs/server-sdk/src/data_components/serialization_adapters/json_destination.cpp
@@ -5,65 +5,95 @@
 
 namespace launchdarkly::server_side::data_components {
 
+using data_interfaces::ISerializedDestination;
 using integrations::SerializedItemDescriptor;
 
 FlagKind const JsonDestination::Kinds::Flag = FlagKind();
 SegmentKind const JsonDestination::Kinds::Segment = SegmentKind();
 
-JsonDestination::JsonDestination(
-    data_interfaces::ISerializedDestination& destination)
+/**
+ * @brief Creates a boost::json::value representing a tombstone for a given
+ * ItemDescriptor. The tombstone includes 'deleted', 'key', and 'version'
+ * fields.
+ *
+ * @param T Type of descriptor.
+ * @param key Key of item.
+ * @param desc The descriptor.
+ * @return Tombstone suitable for serialization.
+ */
+template <typename T>
+boost::json::value Tombstone(std::string const& key,
+                             data_model::ItemDescriptor<T> const& desc) {
+    boost::json::object tombstone;
+    tombstone.emplace("deleted", true);
+    tombstone.emplace("key", key);
+    tombstone.emplace("version", desc.version);
+    return tombstone;
+}
+
+/**
+ * @brief Creates a SerializedItemDescriptor for a given ItemDescriptor. The
+ * SerializedItemDescriptor either represents an item that is present, or one
+ * that is absent.
+ *
+ * @param T Type of descriptor.
+ * @param key Key of item.
+ * @param desc The descriptor.
+ * @return SerializedItemDescriptor suitable for forwarding to an
+ * ISerializedDestination.
+ */
+template <typename T>
+SerializedItemDescriptor Serialize(std::string const& key,
+                                   data_model::ItemDescriptor<T> const& desc) {
+    return desc.item
+               ? SerializedItemDescriptor::Present(
+                     desc.version, boost::json::serialize(
+                                       boost::json::value_from(*desc.item)))
+               : SerializedItemDescriptor::Absent(
+                     desc.version,
+                     boost::json::serialize(Tombstone(key, desc)));
+}
+
+JsonDestination::JsonDestination(ISerializedDestination& destination)
     : dest_(destination) {}
 
 void JsonDestination::Init(data_model::SDKDataSet data_set) {
     // TODO: implement
+
+    std::vector<ISerializedDestination::ItemCollection> items;
+
+    ISerializedDestination::OrderedNamepace flags;
+    for (auto const& [key, descriptor] : data_set.flags) {
+        flags.emplace_back(key, Serialize(key, descriptor));
+    }
+    std::sort(flags.begin(), flags.end());
+    items.emplace_back(Kinds::Flag, std::move(flags));
+
+    ISerializedDestination::OrderedNamepace segments;
+    for (auto const& [key, descriptor] : data_set.segments) {
+        segments.emplace_back(key, Serialize(key, descriptor));
+    }
+    std::sort(segments.begin(), segments.end());
+    items.emplace_back(Kinds::Segment, std::move(segments));
+
+    dest_.Init(std::move(items));
 }
 
 void JsonDestination::Upsert(std::string const& key,
                              data_model::FlagDescriptor flag) {
-    SerializedItemDescriptor descriptor;
-
-    if (!flag.item) {
-        boost::json::object tombstone;
-        tombstone.emplace("deleted", true);
-        tombstone.emplace("key", key);
-        tombstone.emplace("version", flag.version);
-
-        descriptor = SerializedItemDescriptor::Absent(
-            flag.version, boost::json::serialize(tombstone));
-    } else {
-        descriptor = SerializedItemDescriptor::Present(
-            flag.version,
-            boost::json::serialize(boost::json::value_from(*flag.item)));
-    }
-
-    // TOOD: Log upsert errors?
-
-    auto _ = dest_.Upsert(Kinds::Flag, key, std::move(descriptor));
+    // TODO: how to handle errors?
+    dest_.Upsert(Kinds::Flag, key, Serialize(key, flag));
 }
 
 void JsonDestination::Upsert(std::string const& key,
                              data_model::SegmentDescriptor segment) {
-    SerializedItemDescriptor descriptor;
-
-    if (!segment.item) {
-        boost::json::object tombstone;
-        tombstone.emplace("deleted", true);
-        tombstone.emplace("key", key);
-        tombstone.emplace("version", segment.version);
-
-        descriptor = SerializedItemDescriptor::Absent(
-            segment.version, boost::json::serialize(tombstone));
-    } else {
-        descriptor = SerializedItemDescriptor::Present(
-            segment.version,
-            boost::json::serialize(boost::json::value_from(*segment.item)));
-    }
-    // TOOD: Log upsert errors?
-
-    auto _ = dest_.Upsert(Kinds::Segment, key, std::move(descriptor));
+    // TODO: how to handle errors?
+    dest_.Upsert(Kinds::Segment, key, Serialize(key, segment));
 }
 
 std::string const& JsonDestination::Identity() const {
-    return dest_.Identity();
+    static std::string const identity = dest_.Identity() + "(JSON)";
+    return identity;
 }
+
 }  // namespace launchdarkly::server_side::data_components
--- libs/server-sdk/src/data_components/serialization_adapters/json_destination.hpp
+++ libs/server-sdk/src/data_components/serialization_adapters/json_destination.hpp
@@ -6,19 +6,59 @@
 
 namespace launchdarkly::server_side::data_components {
 
+/**
+ * @brief JsonDestination is responsible for converting flag and segment
+ * models into serialized data suitable for storage in an
+ * ISerializedDestination.
+ *
+ * It's purpose is to encapsulate the details of serialization in a reusable
+ * adapter.
+ *
+ */
 class JsonDestination final : public data_interfaces::IDestination {
    public:
+    /**
+     * @brief Construct the JsonDestination with the given destination.
+     * Calls to Upsert will trigger serialization and storage in the
+     * destination.
+     * @param destination Where data should be forwarded.
+     */
     explicit JsonDestination(
         data_interfaces::ISerializedDestination& destination);
 
-    virtual void Init(data_model::SDKDataSet data_set) override;
+    /**
+     * @brief Initialize the destination with an SDK data set.
+     * @param data_set The initial data.
+     */
+    void Init(data_model::SDKDataSet data_set) override;
 
+    /**
+     * @brief Upsert data for the flag named by key.
+     *
+     * If the descriptor represents a deleted item, a tombstone will
+     * be forwarded to the ISerializedDestination.
+     *
+     * @param key Key of flag.
+     * @param flag Descriptor of flag.
+     */
     void Upsert(std::string const& key,
                 data_model::FlagDescriptor flag) override;
 
+    /**
+     * @brief Upsert data for the segment named by key.
+     *
+     * If the descriptor represents a deleted item, a tombstone will
+     * be forwarded to the ISerializedDestination.
+     *
+     * @param key Key of segment.
+     * @param segment Descriptor of segment.
+     */
     void Upsert(std::string const& key,
                 data_model::SegmentDescriptor segment) override;
 
+    /**
+     * @return Identity of this destination. Used in logs.
+     */
     [[nodiscard]] std::string const& Identity() const override;
 
    private:
