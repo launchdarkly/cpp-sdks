ba4c16b3c06f10a7723fbbdad6a595455ff92a59
Casey Waldren <cwaldren@launchdarkly.com>
2023-11-28 17:09:17 -0800

    feat: implement JSON destination (#308)
    This PR adds a JSON adapter (`JsonDestination`) which is capable of
    accepting SDK in-memory models, transforming them into JSON, and passing
    them down to an `ISerializedDataDestination`.
    
    In practice, this will be used to store serialized data in Redis.

--- libs/server-sdk/include/launchdarkly/server_side/integrations/serialized_item_descriptor.hpp
+++ libs/server-sdk/include/launchdarkly/server_side/integrations/serialized_item_descriptor.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 #include <optional>
+#include <ostream>
 #include <string>
 
 namespace launchdarkly::server_side::integrations {
@@ -24,6 +25,47 @@ struct SerializedItemDescriptor {
      * std::nullopt for deleted items.
      */
     std::optional<std::string> serializedItem;
+
+    /**
+     * @brief Constructs a SerializedItemDescriptor from a version and a
+     * serialized item.
+     * @param version Version of item.
+     * @param data Serialized item.
+     * @return SerializedItemDescriptor.
+     */
+    static SerializedItemDescriptor Present(std::uint64_t version,
+                                            std::string data) {
+        return SerializedItemDescriptor{version, false, std::move(data)};
+    }
+
+    /**
+     * @brief Constructs a SerializedItemDescriptor from a version and a
+     * tombstone.
+     *
+     * This is used when an item is deleted: the tombstone can be stored in
+     * place of the item, and the version checked in the future. Without the
+     * tombstone, out-of-order data updates could "resurrect" a deleted item.
+     *
+     * @param version Version of the item.
+     * @param tombstone_rep Serialized tombstone representation of the item.
+     * @return SerializedItemDescriptor.
+     */
+    static SerializedItemDescriptor Absent(std::uint64_t const version,
+                                           std::string tombstone_rep) {
+        return SerializedItemDescriptor{version, true,
+                                        std::move(tombstone_rep)};
+    }
 };
+
+inline bool operator==(SerializedItemDescriptor const& lhs,
+                       SerializedItemDescriptor const& rhs) {
+    return lhs.version == rhs.version && lhs.deleted == rhs.deleted &&
+           lhs.serializedItem == rhs.serializedItem;
+}
+
+inline void PrintTo(SerializedItemDescriptor const& item, std::ostream* os) {
+    *os << "{version=" << item.version << ", deleted=" << item.deleted
+        << ", item=" << item.serializedItem.value_or("nullopt") << "}";
+}
 
 }  // namespace launchdarkly::server_side::integrations
--- libs/server-sdk/src/data_components/serialization_adapters/json_destination.cpp
+++ libs/server-sdk/src/data_components/serialization_adapters/json_destination.cpp
@@ -1,26 +1,129 @@
 #include "json_destination.hpp"
 
+#include <launchdarkly/serialization/json_flag.hpp>
+#include <launchdarkly/serialization/json_segment.hpp>
+
 namespace launchdarkly::server_side::data_components {
 
-JsonDestination::JsonDestination(
-    data_interfaces::ISerializedDestination& destination)
-    : dest_(destination) {}
+using data_interfaces::ISerializedDestination;
+using integrations::SerializedItemDescriptor;
 
-void JsonDestination::Init(data_model::SDKDataSet data_set) {
-    // TODO: serialize and forward to dest_.Init
+FlagKind const JsonDestination::Kinds::Flag = FlagKind();
+SegmentKind const JsonDestination::Kinds::Segment = SegmentKind();
+
+/**
+ * @brief Creates a boost::json::value representing a tombstone for a given
+ * ItemDescriptor. The tombstone includes 'deleted', 'key', and 'version'
+ * fields.
+ *
+ * @param T Type of descriptor.
+ * @param key Key of item.
+ * @param desc The descriptor.
+ * @return Tombstone suitable for serialization.
+ */
+template <typename T>
+boost::json::value Tombstone(std::string const& key,
+                             data_model::ItemDescriptor<T> const& desc) {
+    boost::json::object tombstone;
+    tombstone.emplace("key", key);
+    tombstone.emplace("version", desc.version);
+    tombstone.emplace("deleted", true);
+    return tombstone;
 }
 
-void JsonDestination::Upsert(std::string const& key,
-                             data_model::FlagDescriptor flag) {
-    // TODO: serialize and forward to dest_.Upsert
+/**
+ * @brief Creates a SerializedItemDescriptor for a given ItemDescriptor. The
+ * SerializedItemDescriptor either represents an item that is present, or one
+ * that is absent.
+ *
+ * @param T Type of descriptor.
+ * @param key Key of item.
+ * @param desc The descriptor.
+ * @return SerializedItemDescriptor suitable for forwarding to an
+ * ISerializedDestination.
+ */
+template <typename T>
+SerializedItemDescriptor Serialize(std::string const& key,
+                                   data_model::ItemDescriptor<T> const& desc) {
+    return desc.item
+               ? SerializedItemDescriptor::Present(
+                     desc.version, boost::json::serialize(
+                                       boost::json::value_from(*desc.item)))
+               : SerializedItemDescriptor::Absent(
+                     desc.version,
+                     boost::json::serialize(Tombstone(key, desc)));
 }
 
-void JsonDestination::Upsert(std::string const& key,
-                             data_model::SegmentDescriptor segment) {
-    // TODO: serialize and forward to dest_.Upsert
-}
+JsonDestination::JsonDestination(Logger const& logger,
+                                 ISerializedDestination& destination)
+    : logger_(logger),
+      dest_(destination),
+      ident_(dest_.Identity() + " (JSON)") {}
 
 std::string const& JsonDestination::Identity() const {
-    return dest_.Identity();
+    return ident_;
 }
+
+void JsonDestination::Init(data_model::SDKDataSet data_set) {
+    // TODO(sc-225327): Topographical sort of flag dependencies
+
+    std::vector<ISerializedDestination::ItemCollection> items;
+
+    ISerializedDestination::OrderedNamepace flags;
+    for (auto const& [key, descriptor] : data_set.flags) {
+        flags.emplace_back(key, Serialize(key, descriptor));
+    }
+    std::sort(flags.begin(), flags.end(), [](auto const& lhs, auto const& rhs) {
+        return lhs.first < rhs.first;
+    });
+    items.emplace_back(Kinds::Flag, std::move(flags));
+
+    ISerializedDestination::OrderedNamepace segments;
+    for (auto const& [key, descriptor] : data_set.segments) {
+        segments.emplace_back(key, Serialize(key, descriptor));
+    }
+    std::sort(
+        segments.begin(), segments.end(),
+        [](auto const& lhs, auto const& rhs) { return lhs.first < rhs.first; });
+    items.emplace_back(Kinds::Segment, std::move(segments));
+
+    if (auto const result = dest_.Init(std::move(items));
+        result != ISerializedDestination::InitResult::kSuccess) {
+        LD_LOG(logger_, LogLevel::kError)
+            << dest_.Identity() << ": failed to store initial SDK data";
+    }
+}
+
+void JsonDestination::Upsert(std::string const& key,
+                             data_model::FlagDescriptor const flag) {
+    LogUpsertResult(key, "flag",
+                    dest_.Upsert(Kinds::Flag, key, Serialize(key, flag)));
+}
+
+void JsonDestination::Upsert(std::string const& key,
+                             data_model::SegmentDescriptor const segment) {
+    LogUpsertResult(key, "segment",
+                    dest_.Upsert(Kinds::Segment, key, Serialize(key, segment)));
+}
+
+void JsonDestination::LogUpsertResult(
+    std::string const& key,
+    std::string const& data_type,
+    ISerializedDestination::UpsertResult const& result) const {
+    switch (result) {
+        case ISerializedDestination::UpsertResult::kSuccess:
+            break;
+        case ISerializedDestination::UpsertResult::kError:
+            LD_LOG(logger_, LogLevel::kError)
+                << dest_.Identity() << ": failed to update " << data_type << " "
+                << key;
+            break;
+        case ISerializedDestination::UpsertResult::kNotUpdated:
+            LD_LOG(logger_, LogLevel::kDebug)
+                << dest_.Identity() << ": " << data_type << " " << key
+                << " not updated; data was stale";
+            break;
+    }
+}
+
 }  // namespace launchdarkly::server_side::data_components
