aee602959aa2192a1c31cafb45f56473943b6c89
Casey Waldren <cwaldren@launchdarkly.com>
2023-11-30 08:55:21 -0800

    fix test

--- libs/server-sdk/tests/redis/redis_source_test.cpp
+++ libs/server-sdk/tests/redis/redis_source_test.cpp
@@ -7,10 +7,14 @@ using namespace launchdarkly::server_side::data_systems;
 using namespace launchdarkly::server_side::data_components;
 
 TEST(RedisTests, ConnectToRedis) {
-    RedisDataSource source("tcp://localhost:6379", "test");
-    ASSERT_FALSE(source.Initialized());
+    auto maybe_source = RedisDataSource::Create("tcp://localhost:6379", "test");
+    ASSERT_TRUE(maybe_source);
 
-    auto all_flags = source.All(FlagKind{});
+    auto const source = *maybe_source;
+
+    ASSERT_FALSE(source->Initialized());
+
+    auto all_flags = source->All(FlagKind{});
     ASSERT_TRUE(all_flags.has_value());
     ASSERT_EQ(all_flags->size(), 0);
 }
