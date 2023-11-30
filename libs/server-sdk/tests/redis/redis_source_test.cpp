7def292bae2596421aee913e4d88e8320b48f1bd
Casey Waldren <cwaldren@launchdarkly.com>
2023-11-30 10:23:16 -0800

    update googletest

--- libs/server-sdk/tests/redis/redis_source_test.cpp
+++ libs/server-sdk/tests/redis/redis_source_test.cpp
@@ -12,25 +12,81 @@ using namespace launchdarkly::server_side::data_systems;
 using namespace launchdarkly::server_side::data_components;
 using namespace launchdarkly::data_model;
 
+class PrefixedClient {
+   public:
+    PrefixedClient(sw::redis::Redis& client, std::string const& prefix)
+        : client_(client), prefix_(prefix) {}
+
+    void Init() const {
+        try {
+            client_.set(prefix_ + ":$inited", "true");
+        } catch (sw::redis::Error const& e) {
+            FAIL() << e.what();
+        }
+    }
+
+    void PutFlag(Flag const& flag) const {
+        try {
+            client_.hset(prefix_ + ":features", flag.key,
+                         serialize(boost::json::value_from(flag)));
+        } catch (sw::redis::Error const& e) {
+            FAIL() << e.what();
+        }
+    }
+
+    void Clear() const {
+        try {
+            std::vector<std::pair<std::string, std::string>> output;
+            client_.keys(prefix_ + ":*", std::back_inserter(output));
+            for (auto const& [key, _] : output) {
+                client_.del(key);
+            }
+        } catch (sw::redis::Error const& e) {
+            FAIL() << e.what();
+        }
+    }
+
+   private:
+    sw::redis::Redis& client_;
+    std::string const& prefix_;
+};
+
 class RedisTests : public ::testing::Test {
    public:
     explicit RedisTests()
         : uri_("tcp://localhost:6379"), prefix_("testprefix"), client_(uri_) {}
 
     void SetUp() override {
+        try {
+            client_.flushdb();
+        } catch (sw::redis::Error const& e) {
+            FAIL() << "couldn't clear Redis: " << e.what();
+        }
+
         auto maybe_source = RedisDataSource::Create(uri_, prefix_);
         ASSERT_TRUE(maybe_source);
         source = std::move(*maybe_source);
     }
 
-    testing::AssertionResult PutFlag(Flag const& flag) {
-        try {
-            client_.hset(prefix_ + ":features", flag.key,
-                         serialize(boost::json::value_from(flag)));
-            return testing::AssertionSuccess();
-        } catch (sw::redis::Error const& e) {
-            return testing::AssertionFailure(testing::Message(e.what()));
-        }
+    void Init() {
+        auto const client = PrefixedClient(client_, prefix_);
+        client.Init();
+    }
+
+    void PutFlag(Flag const& flag) {
+        auto const client = PrefixedClient(client_, prefix_);
+        client.PutFlag(flag);
+    }
+
+    void Clear() {
+        auto const client = PrefixedClient(client_, prefix_);
+        client.Clear();
+    }
+
+    void WithPrefix(std::string const& prefix,
+                    std::function<void(PrefixedClient const&)> const& f) {
+        auto const client = PrefixedClient(client_, prefix);
+        f(client);
     }
 
    protected:
@@ -42,12 +98,47 @@ class RedisTests : public ::testing::Test {
     sw::redis::Redis client_;
 };
 
-TEST_F(RedisTests, Sourc) {
-    ASSERT_TRUE(PutFlag(Flag{"foo", 2, true}));
-
+TEST_F(RedisTests, RedisEmptyIsNotInitialized) {
     ASSERT_FALSE(source->Initialized());
 
     auto all_flags = source->All(FlagKind{});
     ASSERT_TRUE(all_flags.has_value());
-    ASSERT_EQ(all_flags->size(), 0);
+    ASSERT_TRUE(all_flags->empty());
+
+    auto all_segments = source->All(SegmentKind{});
+    ASSERT_TRUE(all_segments.has_value());
+    ASSERT_TRUE(all_flags->empty());
+}
+
+TEST_F(RedisTests, ChecksInitialized) {
+    ASSERT_FALSE(source->Initialized());
+    Init();
+    ASSERT_TRUE(source->Initialized());
+}
+
+TEST_F(RedisTests, GetFlag) {
+    PutFlag(Flag{"foo", 1, true});
+
+    auto result = source->Get(FlagKind{}, "foo");
+    ASSERT_TRUE(result);
+
+    ASSERT_EQ(result->version, 1);
+    ASSERT_FALSE(result->deleted);
+}
+
+TEST_F(RedisTests, ChecksInitializedPrefixIndependence) {
+    WithPrefix("not_our_prefix", [&](auto const& client) {
+        client.Init();
+        ASSERT_FALSE(source->Initialized());
+    });
+
+    WithPrefix("TestPrefix", [&](auto const& client) {
+        client.Init();
+        ASSERT_FALSE(source->Initialized());
+    });
+
+    WithPrefix("stillnotprefix", [&](auto const& client) {
+        client.Init();
+        ASSERT_FALSE(source->Initialized());
+    });
 }
