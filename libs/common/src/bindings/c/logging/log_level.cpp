486021217a06c595da5caa1a8aacc2815b3ba7fd
Casey Waldren <cwaldren@launchdarkly.com>
2023-12-12 15:43:49 -0800

    add symmetric api

--- libs/common/src/bindings/c/logging/log_level.cpp
+++ libs/common/src/bindings/c/logging/log_level.cpp
@@ -2,7 +2,14 @@
 
 #include <launchdarkly/logging/log_level.hpp>
 
-LD_EXPORT(char const*) LDLogLevel_Name(enum LDLogLevel level) {
+LD_EXPORT(char const*)
+LDLogLevel_Name(LDLogLevel level, char const* name_if_unknown) {
     return GetLogLevelName(static_cast<launchdarkly::LogLevel>(level),
-                           "unknown");
+                           name_if_unknown);
+}
+
+LD_EXPORT(LDLogLevel)
+LDLogLevel_Enum(char const* level, LDLogLevel level_if_unknown) {
+    return static_cast<LDLogLevel>(GetLogLevelEnum(
+        level, static_cast<launchdarkly::LogLevel>(level_if_unknown)));
 }
