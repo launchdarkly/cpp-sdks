3c1c8c6b61929443571500a6a9fa6f32cd38104e
Casey Waldren <cwaldren@launchdarkly.com>
2023-12-12 15:43:49 -0800

    add symmetric api

--- libs/common/include/launchdarkly/bindings/c/logging/log_level.h
+++ libs/common/include/launchdarkly/bindings/c/logging/log_level.h
@@ -25,7 +25,25 @@ enum LDLogLevel {
     LD_LOG_ERROR = 3,
 };
 
-LD_EXPORT(char const*) LDLogLevel_Name(enum LDLogLevel level);
+/**
+ * Lookup the name of a LDLogLevel.
+ * @param level Target level.
+ * @param level_if_unknown Default name to return if the level wasn't
+ * recognized.
+ * @return Name of the level as a string, or level_if_unknown if not recognized.
+ */
+LD_EXPORT(char const*)
+LDLogLevel_Name(enum LDLogLevel level, char const* level_if_unknown);
+
+/**
+ * Lookup a LDLogLevel by name.
+ * @param level Name of level.
+ * @param level_if_unknown Default level to return if the level wasn't
+ * recognized.
+ * @return LDLogLevel matching the name, or level_if_unknown if not recognized.
+ */
+LD_EXPORT(enum LDLogLevel)
+LDLogLevel_Enum(char const* level, enum LDLogLevel level_if_unknown);
 
 #ifdef __cplusplus
 }
