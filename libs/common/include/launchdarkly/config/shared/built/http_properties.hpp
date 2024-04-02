2c07469dc8ceae3f4f1aac29b633e3eb56c8f3d5
Casey Waldren <cwaldren@launchdarkly.com>
2024-04-02 11:02:28 -0700

    feat: add TLS verify peer option

--- libs/common/include/launchdarkly/config/shared/built/http_properties.hpp
+++ libs/common/include/launchdarkly/config/shared/built/http_properties.hpp
@@ -9,11 +9,21 @@ namespace launchdarkly::config::shared::built {
 
 class HttpProperties final {
    public:
+    class TLS final {
+       public:
+        TLS(bool verify_peer);
+        [[nodiscard]] bool VerifyPeer() const;
+
+       private:
+        bool verify_peer_;
+    };
+
     HttpProperties(std::chrono::milliseconds connect_timeout,
                    std::chrono::milliseconds read_timeout,
                    std::chrono::milliseconds write_timeout,
                    std::chrono::milliseconds response_timeout,
-                   std::map<std::string, std::string> base_headers);
+                   std::map<std::string, std::string> base_headers,
+                   TLS tls);
 
     [[nodiscard]] std::chrono::milliseconds ConnectTimeout() const;
     [[nodiscard]] std::chrono::milliseconds ReadTimeout() const;
@@ -22,16 +32,21 @@ class HttpProperties final {
     [[nodiscard]] std::chrono::milliseconds ResponseTimeout() const;
     [[nodiscard]] std::map<std::string, std::string> const& BaseHeaders() const;
 
+    [[nodiscard]] class TLS const& TLS() const;
+
    private:
     std::chrono::milliseconds connect_timeout_;
     std::chrono::milliseconds read_timeout_;
     std::chrono::milliseconds write_timeout_;
     std::chrono::milliseconds response_timeout_;
     std::map<std::string, std::string> base_headers_;
+    class TLS tls_;
 
     // TODO: Proxy.
 };
 
 bool operator==(HttpProperties const& lhs, HttpProperties const& rhs);
+bool operator==(class HttpProperties::TLS const& lhs,
+                class HttpProperties::TLS const& rhs);
 
 }  // namespace launchdarkly::config::shared::built
--- libs/common/src/config/http_properties.cpp
+++ libs/common/src/config/http_properties.cpp
@@ -4,16 +4,24 @@
 
 namespace launchdarkly::config::shared::built {
 
+HttpProperties::TLS::TLS(bool verify_peer) : verify_peer_(verify_peer) {}
+
+bool HttpProperties::TLS::VerifyPeer() const {
+    return verify_peer_;
+}
+
 HttpProperties::HttpProperties(std::chrono::milliseconds connect_timeout,
                                std::chrono::milliseconds read_timeout,
                                std::chrono::milliseconds write_timeout,
                                std::chrono::milliseconds response_timeout,
-                               std::map<std::string, std::string> base_headers)
+                               std::map<std::string, std::string> base_headers,
+                               class HttpProperties::TLS tls)
     : connect_timeout_(connect_timeout),
       read_timeout_(read_timeout),
       write_timeout_(write_timeout),
       response_timeout_(response_timeout),
-      base_headers_(std::move(base_headers)) {}
+      base_headers_(std::move(base_headers)),
+      tls_(std::move(tls)) {}
 
 std::chrono::milliseconds HttpProperties::ConnectTimeout() const {
     return connect_timeout_;
@@ -35,11 +43,20 @@ std::map<std::string, std::string> const& HttpProperties::BaseHeaders() const {
     return base_headers_;
 }
 
+class HttpProperties::TLS HttpProperties::TLS() const& {
+    return tls_;
+}
+
 bool operator==(HttpProperties const& lhs, HttpProperties const& rhs) {
     return lhs.ReadTimeout() == rhs.ReadTimeout() &&
            lhs.WriteTimeout() == rhs.WriteTimeout() &&
            lhs.ConnectTimeout() == rhs.ConnectTimeout() &&
-           lhs.BaseHeaders() == rhs.BaseHeaders();
+           lhs.BaseHeaders() == rhs.BaseHeaders() && lhs.TLS() == rhs.TLS();
+}
+
+bool operator==(class HttpProperties::TLS const& lhs,
+                class HttpProperties::TLS const& rhs) {
+    return lhs.VerifyPeer() == rhs.VerifyPeer();
 }
 
 }  // namespace launchdarkly::config::shared::built
