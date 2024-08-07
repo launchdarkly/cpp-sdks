b5db523b844de2ed1436148ace0285f92ecc0801
Casey Waldren <cwaldren@launchdarkly.com>
2024-08-07 12:27:32 -0700

    add separate step for only testing C apis

--- scripts/run-hello-apps.sh
+++ scripts/run-hello-apps.sh
@@ -5,18 +5,26 @@
 # Example:
 # ./scripts/run-hello-apps.sh dynamic hello-cpp-client hello-c-client
 #
-# $1 is the linkage, either 'static' or 'dynamic'.
+# $1 is the linkage, either 'static', 'dynamic', or 'dynamic-export-all-symbols'.
+# The difference between the dynamic options is that 'dynamic' only exports the C API,
+# whereas 'dynamic-export-all-symbols' exports all symbols (including C++, so we can link
+# a dynamic C++ library in the hello example.)
 # Subsequent arguments are cmake target names.
 
-if [ "$1" != "static" ] && [ "$1" != "dynamic" ]
+if [ "$1" != "static" ] && [ "$1" != "dynamic" ] && [ "$1" != "dynamic-export-all-symbols" ]
 then
-  echo "Linkage must be specified ('static' or 'dynamic')"
+  echo "Linkage must be specified ('static', 'dynamic', or 'dynamic-export-all-symbols')"
   exit 1
 fi
 
 dynamic_linkage="Off"
+export_all_symbols="Off"
+
 if [ "$1" == "dynamic" ]; then
     dynamic_linkage="On"
+elif [ "$1" == "dynamic-export-all-symbols" ]; then
+    dynamic_linkage="On"
+    export_all_symbols="On"
 fi
 
 shift
@@ -35,7 +43,7 @@ trap cleanup EXIT
 cmake -G Ninja -D CMAKE_BUILD_TYPE=Release \
                -D BUILD_TESTING=OFF  \
                -D LD_BUILD_SHARED_LIBS=$dynamic_linkage \
-               -D LD_BUILD_EXPORT_ALL_SYMBOLS=ON ..
+               -D LD_BUILD_EXPORT_ALL_SYMBOLS=$export_all_symbols ..
 
 export LD_MOBILE_KEY="bogus"
 export LD_SDK_KEY="bogus"
