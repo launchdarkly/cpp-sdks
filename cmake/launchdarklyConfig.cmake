e9a3e59c55da77a6efd1c9914b6bce431bcac717
Casey Waldren <cwaldren@launchdarkly.com>
2024-10-07 11:35:20 -0700

    set BOOST_USE_MULTITHREADED and BOOST_USE_STATIC_RUNTIME in launchdarklyConfig

--- cmake/launchdarklyConfig.cmake
+++ cmake/launchdarklyConfig.cmake
@@ -1,5 +1,9 @@
 include(CMakeFindDependencyMacro)
 
+set(Boost_USE_MULTITHREADED ON)
+set(Boost_USE_STATIC_RUNTIME OFF)
+
+
 find_dependency(Boost 1.81 COMPONENTS json url coroutine)
 find_dependency(OpenSSL)
 find_dependency(tl-expected)
