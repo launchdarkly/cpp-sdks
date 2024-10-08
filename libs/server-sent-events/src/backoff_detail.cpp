ab8f150cc7c510065ed1c66408783a3aa7165421
Casey Waldren <cwaldren@launchdarkly.com>
2024-10-08 14:30:05 -0700

    make implementation of detail methods easier to read

--- libs/server-sent-events/src/backoff_detail.cpp
+++ libs/server-sent-events/src/backoff_detail.cpp
@@ -6,14 +6,16 @@ std::chrono::milliseconds calculate_backoff(
     std::uint64_t const attempt,
     std::uint64_t const max_exponent) {
     std::uint64_t const exponent = std::min(attempt - 1, max_exponent);
-    return initial * static_cast<uint64_t>(std::pow(2, exponent));
+    double const exponentiated = std::pow(2, exponent);
+    return initial * static_cast<uint64_t>(exponentiated);
 }
 
 std::chrono::milliseconds jitter(std::chrono::milliseconds const base,
                                  double const jitter_ratio,
                                  std::function<double(double)> const& random) {
-    return std::chrono::milliseconds(static_cast<uint64_t>(
-        base.count() - (random(jitter_ratio) * base.count())));
+    double const jitter = random(jitter_ratio);
+    double const jittered_base = base.count() - (jitter * base.count());
+    return std::chrono::milliseconds(static_cast<uint64_t>(jittered_base));
 }
 
 std::chrono::milliseconds delay(std::chrono::milliseconds const initial,
@@ -22,8 +24,8 @@ std::chrono::milliseconds delay(std::chrono::milliseconds const initial,
                                 std::uint64_t const max_exponent,
                                 double const jitter_ratio,
                                 std::function<double(double)> const& random) {
-    return jitter(
-        std::min(calculate_backoff(initial, attempt, max_exponent), max),
-        jitter_ratio, random);
+    auto const backoff = calculate_backoff(initial, attempt, max_exponent);
+    auto const constrained_backoff = std::min(backoff, max);
+    return jitter(constrained_backoff, jitter_ratio, random);
 }
-}  // namespace detail
+}  // namespace launchdarkly::sse::detail
