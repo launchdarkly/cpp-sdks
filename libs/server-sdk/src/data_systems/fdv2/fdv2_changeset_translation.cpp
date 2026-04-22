09b2ef44982f9c6cef7e555bd61a28407bee7476
Bee Klimt <bklimt@launchdarkly.com>
2026-04-22 10:24:08 -0700

    refactor: split up a long function into a few smaller ones

--- libs/server-sdk/src/data_systems/fdv2/fdv2_changeset_translation.cpp
+++ libs/server-sdk/src/data_systems/fdv2/fdv2_changeset_translation.cpp
@@ -17,6 +17,68 @@ using data_model::ChangeSet;
 using data_model::ChangeSetType;
 using data_model::FDv2ChangeSet;
 
+static std::optional<ItemChange> TranslateDelete(
+    data_model::FDv2Change const& change,
+    Logger const& logger) {
+    if (change.kind == "flag") {
+        return ItemChange{change.key,
+                          data_model::ItemDescriptor<data_model::Flag>{
+                              data_model::Tombstone{change.version}}};
+    }
+    if (change.kind == "segment") {
+        return ItemChange{change.key,
+                          data_model::ItemDescriptor<data_model::Segment>{
+                              data_model::Tombstone{change.version}}};
+    }
+    LD_LOG(logger, LogLevel::kWarn) << "FDv2: unknown kind '" << change.kind
+                                    << "' in delete-object, skipping";
+    return std::nullopt;
+}
+
+static bool TranslatePutFlag(data_model::FDv2Change const& change,
+                             ChangeSetData* changes,
+                             Logger const& logger) {
+    auto result = boost::json::value_to<
+        tl::expected<std::optional<data_model::Flag>, JsonError>>(
+        change.object);
+    if (!result) {
+        LD_LOG(logger, LogLevel::kError)
+            << "FDv2: could not deserialize flag '" << change.key << "'";
+        return false;
+    }
+    if (!result->has_value()) {
+        LD_LOG(logger, LogLevel::kWarn)
+            << "FDv2: flag '" << change.key << "' object was null, skipping";
+        return true;
+    }
+    changes->push_back(ItemChange{
+        change.key,
+        data_model::ItemDescriptor<data_model::Flag>{std::move(**result)}});
+    return true;
+}
+
+static bool TranslatePutSegment(data_model::FDv2Change const& change,
+                                ChangeSetData* changes,
+                                Logger const& logger) {
+    auto result = boost::json::value_to<
+        tl::expected<std::optional<data_model::Segment>, JsonError>>(
+        change.object);
+    if (!result) {
+        LD_LOG(logger, LogLevel::kError)
+            << "FDv2: could not deserialize segment '" << change.key << "'";
+        return false;
+    }
+    if (!result->has_value()) {
+        LD_LOG(logger, LogLevel::kWarn)
+            << "FDv2: segment '" << change.key << "' object was null, skipping";
+        return true;
+    }
+    changes->push_back(ItemChange{
+        change.key,
+        data_model::ItemDescriptor<data_model::Segment>{std::move(**result)}});
+    return true;
+}
+
 std::optional<ChangeSet<ChangeSetData>> TranslateChangeSet(
     FDv2ChangeSet const& change_set,
     Logger const& logger) {
@@ -30,58 +92,18 @@ std::optional<ChangeSet<ChangeSetData>> TranslateChangeSet(
 
     for (auto const& change : change_set.changes) {
         if (change.change_type == data_model::FDv2Change::ChangeType::kDelete) {
-            if (change.kind == "flag") {
-                changes.push_back(ItemChange{
-                    change.key, data_model::ItemDescriptor<data_model::Flag>{
-                                    data_model::Tombstone{change.version}}});
-            } else if (change.kind == "segment") {
-                changes.push_back(ItemChange{
-                    change.key, data_model::ItemDescriptor<data_model::Segment>{
-                                    data_model::Tombstone{change.version}}});
-            } else {
-                LD_LOG(logger, LogLevel::kWarn)
-                    << "FDv2: unknown kind '" << change.kind
-                    << "' in delete-object, skipping";
+            if (auto item = TranslateDelete(change, logger)) {
+                changes.push_back(std::move(*item));
             }
         } else {
             if (change.kind == "flag") {
-                auto result = boost::json::value_to<
-                    tl::expected<std::optional<data_model::Flag>, JsonError>>(
-                    change.object);
-                if (!result) {
-                    LD_LOG(logger, LogLevel::kError)
-                        << "FDv2: could not deserialize flag '" << change.key
-                        << "'";
+                if (!TranslatePutFlag(change, &changes, logger)) {
                     return std::nullopt;
                 }
-                if (!result->has_value()) {
-                    LD_LOG(logger, LogLevel::kWarn)
-                        << "FDv2: flag '" << change.key
-                        << "' object was null, skipping";
-                    continue;
-                }
-                changes.push_back(ItemChange{
-                    change.key, data_model::ItemDescriptor<data_model::Flag>{
-                                    std::move(**result)}});
             } else if (change.kind == "segment") {
-                auto result = boost::json::value_to<tl::expected<
-                    std::optional<data_model::Segment>, JsonError>>(
-                    change.object);
-                if (!result) {
-                    LD_LOG(logger, LogLevel::kError)
-                        << "FDv2: could not deserialize segment '" << change.key
-                        << "'";
+                if (!TranslatePutSegment(change, &changes, logger)) {
                     return std::nullopt;
                 }
-                if (!result->has_value()) {
-                    LD_LOG(logger, LogLevel::kWarn)
-                        << "FDv2: segment '" << change.key
-                        << "' object was null, skipping";
-                    continue;
-                }
-                changes.push_back(ItemChange{
-                    change.key, data_model::ItemDescriptor<data_model::Segment>{
-                                    std::move(**result)}});
             } else {
                 LD_LOG(logger, LogLevel::kWarn)
                     << "FDv2: unknown kind '" << change.kind
